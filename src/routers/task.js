c9e524d7df3eda21079ed8289d1c7545cfde5358
AnotherStupidDev <fx014@yandex.ru>
2020-02-06 02:29:48 +0500

    Added simple sorting, filtering and paginating data

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,12 +17,34 @@ router.post('/tasks', auth, async (req,res) => {
     }
 })
 
+// GET /tasks?completed=false 
+// GET /tasks?limit=10&skip=0
+// GET /tasks?sortBy=description:desc
 router.get('/tasks', auth, async (req,res) => {
+    const match = {}
+    const sort = {}
 
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
+    
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+    
     try {
-        // Alternative: await req.user.populate('tasks').execPopulate()
-        const tasks = await Task.find({owner: req.user._id})
-        res.send(tasks)
+        // Alternative: const tasks = await Task.find({owner: req.user._id})
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip:parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate()
+        res.send(req.user.tasks)
     } catch (error) {
         res.status(500).send(error.message)
     }
